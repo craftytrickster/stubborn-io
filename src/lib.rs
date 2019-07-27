e5b1a9b2f0117ce425ce3b37ee4f8bd1e269f44c
David Raifaizen <d-raif@outlook.com>
2019-07-27 19:41:28 -0400

    Having scratch app working

--- src/lib.rs
+++ src/lib.rs
@@ -7,38 +7,50 @@ use std::pin::Pin;
 use std::io;
 use tokio::io::{AsyncRead, AsyncWrite};
 use bytes::{Buf, BufMut};
-use std::ops::Deref;
+
 
 pub struct StubbornTcpStream {
-    tcp: TcpStream
+    status: Status,
+    addr: SocketAddr,
+    // maybe always keep stream ref here, and then just mem_swap it when necessary?
 }
 
-impl Deref for StubbornTcpStream {
-    type Target = TcpStream;
-
-    fn deref(&self) -> &Self::Target {
-        &self.tcp
-    }
+enum Status {
+    Connected(TcpStream),
+    Disconnected
 }
+
+//impl Deref for StubbornTcpStream {
+//    type Target = TcpStream;
+//
+//    fn deref(&self) -> &Self::Target {
+//        &self.tcp
+//    }
+//}
 
 impl StubbornTcpStream {
     pub async fn connect(addr: &SocketAddr) -> io::Result<Self> {
         let tcp = TcpStream::connect(addr).await?;
-        Ok(StubbornTcpStream { tcp })
-    }
+        let status = Status::Connected(tcp);
 
-    pub fn poll_peek(&mut self, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<io::Result<usize>> {
-        self.tcp.poll_peek(cx, buf)
+        Ok(StubbornTcpStream { status, addr: *addr })
     }
 }
-
-
 
 // ===== impl Read / Write =====
 
 impl AsyncRead for StubbornTcpStream {
     unsafe fn prepare_uninitialized_buffer(&self, buf: &mut [u8]) -> bool {
-        self.tcp.prepare_uninitialized_buffer(buf)
+        match &self.status {
+            Status::Connected(tcp) => {
+                println!("C:prepare_uninitialized_buffer");
+                tcp.prepare_uninitialized_buffer(buf)
+            },
+            Status::Disconnected => {
+                println!("D:prepare_uninitialized_buffer");
+                false
+            }
+        }
     }
 
     fn poll_read(
@@ -46,7 +58,16 @@ impl AsyncRead for StubbornTcpStream {
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
-        AsyncRead::poll_read(Pin::new(&mut self.tcp), cx, buf)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_read");
+                AsyncRead::poll_read(Pin::new(tcp), cx, buf)
+            },
+            Status::Disconnected => {
+                println!("D:poll_read");
+                Poll::Pending
+            }
+        }
     }
 
     fn poll_read_buf<B: BufMut>(
@@ -54,7 +75,16 @@ impl AsyncRead for StubbornTcpStream {
         cx: &mut Context<'_>,
         buf: &mut B,
     ) -> Poll<io::Result<usize>> {
-        AsyncRead::poll_read_buf(Pin::new(&mut self.tcp), cx, buf)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_read_buf");
+                AsyncRead::poll_read_buf(Pin::new(tcp), cx, buf)
+            },
+            Status::Disconnected => {
+                println!("D:poll_read_buf");
+                Poll::Pending
+            }
+        }
     }
 }
 
@@ -64,17 +94,42 @@ impl AsyncWrite for StubbornTcpStream {
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
-        AsyncWrite::poll_write(Pin::new(&mut self.tcp), cx, buf)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_write");
+                AsyncWrite::poll_write(Pin::new(tcp), cx, buf)
+            },
+            Status::Disconnected => {
+                println!("D:poll_write");
+                Poll::Pending
+            }
+        }
     }
 
-    #[inline]
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        // tcp flush is a no-op
-        AsyncWrite::poll_flush(Pin::new(&mut self.tcp), cx)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_flush");
+                AsyncWrite::poll_flush(Pin::new(tcp), cx)
+            },
+            Status::Disconnected => {
+                println!("D:poll_flush");
+                Poll::Pending
+            }
+        }
     }
 
     fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        AsyncWrite::poll_shutdown(Pin::new(&mut self.tcp), cx)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_shutdown");
+                AsyncWrite::poll_shutdown(Pin::new(tcp), cx)
+            },
+            Status::Disconnected => {
+                println!("D:poll_shutdown");
+                Poll::Pending
+            }
+        }
     }
 
     fn poll_write_buf<B: Buf>(
@@ -82,6 +137,15 @@ impl AsyncWrite for StubbornTcpStream {
         cx: &mut Context<'_>,
         buf: &mut B,
     ) -> Poll<io::Result<usize>> {
-        AsyncWrite::poll_write_buf(Pin::new(&mut self.tcp), cx, buf)
+        match &mut self.status {
+            Status::Connected(ref mut tcp) => {
+                println!("C:poll_write_buf");
+                AsyncWrite::poll_write_buf(Pin::new(tcp), cx, buf)
+            },
+            Status::Disconnected => {
+                println!("D:poll_write_buf");
+                Poll::Pending
+            }
+        }
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+#![feature(async_await)]
+
+use stubborn_stream::StubbornTcpStream;
+use tokio;
+use std::net::{SocketAddr, SocketAddrV4};
+use tokio::io::AsyncWrite;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Poll, Context};
+use std::thread;
+use std::io::{stdin};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::codec::{Framed, LinesCodec};
+use futures::{Sink, Stream};
+
+
+struct MainLoop {
+    framed: Framed<StubbornTcpStream, LinesCodec>,
+    rx: UnboundedReceiver<String>
+}
+
+impl MainLoop {
+    fn new(stub: StubbornTcpStream, rx: UnboundedReceiver<String>) -> Self {
+        let framed = Framed::new(stub, LinesCodec::new());
+        
+        MainLoop { framed, rx }
+    }
+}
+
+impl Future for MainLoop {
+    type Output = ();
+    
+    fn poll(mut self: Pin<& mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        self.rx.poll_recv(cx).map(|item| {
+            Pin::new(&mut self.framed).start_send(item.unwrap());
+            Pin::new(&mut self.framed).poll_flush(cx);
+
+
+            cx.waker().wake_by_ref();
+        });
+
+
+        match Pin::new(&mut self.framed).poll_next(cx) {
+            Poll::Ready(Some(item)) => {
+                println!("FROM SERVER: {:?}", item);
+                cx.waker().wake_by_ref();
+            },
+            Poll::Ready(None) => {
+                println!("YOU ARE NOTHING");
+            }
+            Poll::Pending => {
+//                println!("GRound control to pending tom");
+            }
+        };
+        
+        Poll::Pending
+    }
+}
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut tx, rx) = mpsc::unbounded_channel();
+    thread::spawn(move || {
+        loop {
+            let mut line = String::new();
+            stdin().read_line(&mut line);
+            let _ = tx.try_send(line);
+        }
+    });
+
+
+
+    let addr: SocketAddrV4 = "127.0.0.1:2000".parse().unwrap();
+    let addr = SocketAddr::V4(addr);
+    
+    let connection = StubbornTcpStream::connect(&addr).await.expect("Where's the connection");
+    let thing = MainLoop::new(connection, rx);
+
+    thing.await;
+    Ok(())
+}
